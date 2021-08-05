1e8acbf41c56af86d124140aa945df6e2f3623f4
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-08-05 03:17:42 -0700

    Initialize watchy

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,20 +1,11 @@
-#include <BLE.h>
-#include <bma.h>
-#include <bma4.h>
-#include <bma423.h>
-#include <bma4_defs.h>
-#include <config.h>
-#include <DSEG7_Classic_Bold_53.h>
 #include <Watchy.h>
 
 Watchy watch;
 
 void setup() {
-  // put your setup code here, to run once:
-
+  watch.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
