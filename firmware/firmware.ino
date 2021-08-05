4b95b860cb703f6fdb8c100b5b35b883f1c870c9
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-08-05 03:01:52 -0700

    Initial commit

---
+++ firmware/firmware.ino
@@ -0,0 +1,20 @@
+#include <BLE.h>
+#include <bma.h>
+#include <bma4.h>
+#include <bma423.h>
+#include <bma4_defs.h>
+#include <config.h>
+#include <DSEG7_Classic_Bold_53.h>
+#include <Watchy.h>
+
+Watchy watch;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
