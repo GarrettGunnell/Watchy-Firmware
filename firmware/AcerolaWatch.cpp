5d9bf803cce91b5090686a884cd8f603695c80d1
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-08-05 04:22:09 -0700

    Draw aiko

--- firmware/AcerolaWatch.cpp
+++ firmware/AcerolaWatch.cpp
@@ -1,3 +1,8 @@
 #include "AcerolaWatch.h"
 
 AcerolaWatch::AcerolaWatch(){}
+
+void AcerolaWatch::drawWatchFace() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, epd_bitmap_aiko_icon_1, 200, 200, GxEPD_WHITE);
+}
--- firmware/AcerolaWatch.h
+++ firmware/AcerolaWatch.h
@@ -2,11 +2,12 @@
 #define ACEROLA_WATCH_H
 
 #include <Watchy.h>
-
+#include "aiko_icon_1.h"
 
 class AcerolaWatch : public Watchy {
   public:
     AcerolaWatch();
+    void drawWatchFace();
 };
 
 #endif
