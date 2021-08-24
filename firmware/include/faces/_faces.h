d89ddecc5fa1579c20d4e6611442ff9d970a667c
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-08-23 18:29:38 -0700

    Implement face 91

--- firmware/include/faces/_faces.h
+++ firmware/include/faces/_faces.h
@@ -91,5 +91,6 @@
 #include "88.h"
 #include "89.h"
 #include "90.h"
+#include "91.h"
 
 #endif
--- firmware/src/AcerolaWatch.cpp
+++ firmware/src/AcerolaWatch.cpp
@@ -3,7 +3,7 @@
 AcerolaWatch::AcerolaWatch(){}
 
 void AcerolaWatch::drawWatchFace() {
-  Face90();
+  Face91();
 }
 
 void AcerolaWatch::Face1() {
@@ -1733,4 +1733,19 @@ void AcerolaWatch::Face90() {
   display.print(String(currentTime.Month) + "/");
   display.print((currentTime.Day < 10) ? "0" : "");
   display.println(currentTime.Day);
+}
+
+void AcerolaWatch::Face91() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, watchface91_bg, 200, 200, GxEPD_WHITE);
+  display.setTextColor(GxEPD_WHITE);
+  display.setFont(&Compacta_Bold_Plain_Regular20pt7b);
+  display.setCursor(78, 140);
+  int8_t hour = (currentTime.Hour % 12 != 0) ? currentTime.Hour % 12 : 12;
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour));
+  display.setCursor(93, 170);
+  display.setFont(&Compacta_Bold_Plain_Regular20pt7b);
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
 }
