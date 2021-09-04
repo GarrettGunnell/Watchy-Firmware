06646a63bbe8fd12a3c7bc849bc427df2a98c0d2
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-09-03 19:18:57 -0700

    Implement face 131

--- firmware/src/AcerolaWatch.cpp
+++ firmware/src/AcerolaWatch.cpp
@@ -3,7 +3,7 @@
 AcerolaWatch::AcerolaWatch(){}
 
 void AcerolaWatch::drawWatchFace() {
-  Face123();
+  Face131();
 }
 
 void AcerolaWatch::Face1() {
@@ -2542,6 +2542,44 @@ void AcerolaWatch::Face129() {
 void AcerolaWatch::Face130() {
   display.fillScreen(GxEPD_BLACK);
   display.drawBitmap(0, 0, WatchFaceBg130, 200, 200, GxEPD_WHITE);
+  display.setTextColor(GxEPD_WHITE);
+  display.setFont(&VT323_Regular16pt7b);
+  display.setCursor(5, 195);
+  int8_t hour = (currentTime.Hour % 12 != 0) ? currentTime.Hour % 12 : 12;
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setFont(&VT323_Regular16pt7b);
+  display.setCursor(130, 195);
+  display.print((currentTime.Month < 10) ? "0" : "");
+  display.print(String(currentTime.Month) + "/");
+  display.print((currentTime.Day < 10) ? "0" : "");
+  display.println(currentTime.Day);
+}
+
+void AcerolaWatch::Face131() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, WatchFaceBg131, 200, 200, GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&VT323_Regular14pt7b);
+  display.setCursor(143, 61);
+  int8_t hour = (currentTime.Hour % 12 != 0) ? currentTime.Hour % 12 : 12;
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setTextColor(GxEPD_WHITE);
+  display.setCursor(141, 60);
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+}
+
+void AcerolaWatch::Face132() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, WatchFaceBg132, 200, 200, GxEPD_WHITE);
   display.setTextColor(GxEPD_WHITE);
   display.setFont(&VT323_Regular16pt7b);
   display.setCursor(5, 195);
