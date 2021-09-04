c11b85f8bdaa384404ae748efc81447fbab3101e
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-09-03 23:17:45 -0700

    Implement 144

--- firmware/src/AcerolaWatch.cpp
+++ firmware/src/AcerolaWatch.cpp
@@ -3,7 +3,7 @@
 AcerolaWatch::AcerolaWatch(){}
 
 void AcerolaWatch::drawWatchFace() {
-  Face143();
+  Face144();
 }
 
 void AcerolaWatch::Face1() {
@@ -2822,4 +2822,24 @@ void AcerolaWatch::Face143() {
   display.setCursor(10, 185);
   display.print((currentTime.Minute < 10) ? "0" : "");
   display.print(currentTime.Minute);
+}
+
+void AcerolaWatch::Face144() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, WatchFaceBg144, 200, 200, GxEPD_WHITE);
+  display.setTextColor(GxEPD_WHITE);
+  display.setFont(&VT323_Regular20pt7b);
+  display.setCursor(116, 25);
+  int8_t hour = (currentTime.Hour % 12 != 0) ? currentTime.Hour % 12 : 12;
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setFont(&VT323_Regular14pt7b);
+  display.setTextColor(GxEPD_WHITE);
+  display.setCursor(140, 45);
+  display.print((currentTime.Month < 10) ? "0" : "");
+  display.print(String(currentTime.Month) + "/");
+  display.print((currentTime.Day < 10) ? "0" : "");
+  display.println(currentTime.Day);
 }
