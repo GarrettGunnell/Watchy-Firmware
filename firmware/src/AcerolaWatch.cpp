d5512c7ed44ff3cfa0103105668dd0245cd229b4
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-09-06 19:29:35 -0700

    Implement 169

--- firmware/src/AcerolaWatch.cpp
+++ firmware/src/AcerolaWatch.cpp
@@ -3,7 +3,7 @@
 AcerolaWatch::AcerolaWatch(){}
 
 void AcerolaWatch::drawWatchFace() {
-  Face168();
+  Face169();
 }
 
 void AcerolaWatch::Face1() {
@@ -3291,6 +3291,40 @@ void AcerolaWatch::Face168() {
   display.print(currentTime.Minute);
   display.setFont(&VT323_Regular12pt7b);
   display.setCursor(153, 195);
+  display.print((currentTime.Month < 10) ? "0" : "");
+  display.print(String(currentTime.Month) + "/");
+  display.print((currentTime.Day < 10) ? "0" : "");
+  display.println(currentTime.Day);
+}
+
+void AcerolaWatch::Face169() {
+  display.fillScreen(GxEPD_BLACK);
+  display.drawBitmap(0, 0, WatchFaceBg169, 200, 200, GxEPD_WHITE);
+  display.setTextColor(GxEPD_WHITE);
+  display.setFont(&LinLibertine_M20pt7b);
+  display.setCursor(35, 40);
+  int8_t hour = (currentTime.Hour % 12 != 0) ? currentTime.Hour % 12 : 12;
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setTextColor(GxEPD_WHITE);
+  display.setFont(&LinLibertine_M20pt7b);
+  display.setCursor(39, 40);
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&LinLibertine_M20pt7b);
+  display.setCursor(37, 40);
+  display.print((hour < 10) ? "0" : "");
+  display.print(String(hour) + ":");
+  display.print((currentTime.Minute < 10) ? "0" : "");
+  display.print(currentTime.Minute);
+  display.setFont(&VT323_Regular12pt7b);
+  display.setTextColor(GxEPD_WHITE);
+  display.setCursor(77, 195);
   display.print((currentTime.Month < 10) ? "0" : "");
   display.print(String(currentTime.Month) + "/");
   display.print((currentTime.Day < 10) ? "0" : "");
